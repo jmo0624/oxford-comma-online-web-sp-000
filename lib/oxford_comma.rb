fd15450ed522bc1669d55906d399b339365b1af3
Josh Morris <jmo0624@gmail.com>
2019-05-04 21:45:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   else
-    return array[0..-2].join(', ') + ", and " + array[-10]
+    return array[0..-2].join(', ') + ", and " + array[-1]
   end
 end
